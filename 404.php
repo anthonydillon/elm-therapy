aa274e6e9b41cce1bcaff96e198f3cca824a1b7c
Anthony Dillon <me@anthonydillon.com>
2018-03-02 21:43:42 +0000

    Add 404 page

---
+++ 404.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The default template for displaying content
+ *
+ * Used for both single and index/archive/search.
+ * 
+ * @package ElmTherapy
+ * @since 1.0
+ * @version 1.0
+ */
+get_header(); ?>
+
+<div class="p-strip">
+  <div class="row">
+    <div class="col-12 suffix-2 prefix-2 u-align--center">
+      <h1>Page not found: 404</h1>
+    </div>
+  </div>
+</div>
+
+<?php get_footer(); ?>
